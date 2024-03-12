3f3a42ff57e09abffe65d3f8158450c424a13b1c
venu kommineni <venukommineni615@gmail.com>
2024-03-12 13:14:29 +0530

    created todos component and form component for adding the todos

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <ul>
+        <li>
+            <p>todo</p>
+            <button>delete</button>
+        </li>
+    </ul>
+  )
+}
+
+export default Todos
